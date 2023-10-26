67255c5ade79a092610f028b536b9df4b91ac3b2
Lucas Lima <limalucas7027@gmail.com>
2023-10-25 21:02:15 -0300

    Function watch_list changed

--- auctions/views.py
+++ auctions/views.py
@@ -65,6 +65,8 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def new_listing(request):
+
+    # Check the New Listing form
     if request.method == "POST":
         title = request.POST["title"]
         description = request.POST["description"]
@@ -82,10 +84,27 @@ def new_listing(request):
         starting_bid.save()
         return HttpResponseRedirect(reverse("index"))
 
+    # Render the New Listing form 
     return render(request, "auctions/new_listing.html")
 
 def detail(request, item_title):
     detailed_item = Bid.objects.get(item__title = item_title)
+
+    username = request.user.username
+    current_user = User.objects.get(username=username)
+    watch_list = current_user.watch_list.all()
+    
+    if not watch_list.exists():
+        in_watch_list = False
+    else:
+        # Check if item is in Watch List !!!!!!!!!!!!!!!!ERROR HERE!!!!!!!!!!!!!!!!!!
+        for auction in watch_list:
+            if detailed_item.item.title == auction.item.title:
+                in_watch_list = True
+            else:
+                in_watch_list = False 
+
+    # Check if bid is valid
     if request.method == "POST":
         actual_bid = detailed_item.bid_value
         bid_recived = float(request.POST["bid"])
@@ -104,8 +123,10 @@ def detail(request, item_title):
         "message": message
     })
     
+    # Render detail view
     return render(request, "auctions/detail.html",{
-        "detailed_item": detailed_item
+        "detailed_item": detailed_item,
+        "in_watch_list": in_watch_list
     })
 
 def watch_list(request):
@@ -113,9 +134,31 @@ def watch_list(request):
     current_user = User.objects.get(username=username)
     watch_list = current_user.watch_list.all()
 
+    # Add or remove item from Watch List
+    if request.method == "POST":
+        item = request.POST["item"]
+        if request.POST["action"] == "add":
+           current_user.watch_list.add(Bid.objects.get(item__title = item))
+           current_user.save()
+           return HttpResponseRedirect(reverse('detail', kwargs={'item_title': item}))
+        else:
+            current_user.watch_list.remove(Bid.objects.get(item__title = item))
+            current_user.save()
+            return HttpResponseRedirect(reverse('detail', kwargs={'item_title': item}))
+
+    # Get the User and Watch List
+    
     return render(request, "auctions/watch_list.html", {
         "watch_list": watch_list
     })
+
+    
+
+    
+    
+
+
+    
 
     
 
